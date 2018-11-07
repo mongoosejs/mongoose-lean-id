21728ccfd0cfdd061d425f5a5b64174aedc1a6c7
chris.peters <chris.peters@thrivelearning.co.uk>
2018-11-07 17:21:08 +0000

    Added support for adding id to nested arrays of objects

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ function attachId(res) {
     return
   }
 
-  if (this._mongooseOptions.lean) {
+  function replaceId(res) {
     if (Array.isArray(res)) {
-      res.forEach(function(v) { v.id = v._id.toString() });
+      res.forEach(v => {
+        v.id = v._id.toString();
+        Object.keys(v).map(k => {
+          if (Array.isArray(v[k])) {
+            replaceId(v[k]);
+          }
+        });
+      });
     } else {
       res.id = res._id.toString();
+      Object.keys(res).map(k => {
+        if (Array.isArray(res[k])) {
+          replaceId(res[k]);
+        }
+      });
     }
+  }
+
+  if (this._mongooseOptions.lean) {
+    replaceId(res);
   }
 }
--- test/examples.test.js
+++ test/examples.test.js
@@ -28,4 +28,35 @@ describe('examples', function() {
           findOneAndUpdateRes._id.toHexString());
       });
   });
+
+  it('attaches `.id` to result of find, findOne, and findOneAndUpdate if lean, with nested arrays', function() {
+    const schema = new mongoose.Schema({
+      name: String,
+      objects: [new mongoose.Schema({
+        name: String,
+      })],
+    });
+
+    schema.plugin(mongooseLeanId);
+
+    const Model = mongoose.model('Test2', schema);
+
+    return Model.create({ name: 'test', objects: [{ name: 'test2' }] }).
+      then(() => Promise.all([
+        Model.find().lean(),
+        Model.findOne().lean(),
+        Model.findOneAndUpdate({}, { name: 'test', objects: [{ name: 'test2' }, { name: 'test3' }] }).lean()
+      ])).
+      then(results => {
+        Model.deleteMany();const [findRes, findOneRes, findOneAndUpdateRes] = results;
+        assert.equal(findRes[0].id, findRes[0]._id.toHexString());
+        assert.equal(findRes[0].objects[0].id, findRes[0].objects[0]._id.toHexString());
+        assert.equal(findOneRes.objects[0].id, findOneRes.objects[0]._id.toHexString());
+        assert.equal(findOneAndUpdateRes.objects[0].id,
+          findOneAndUpdateRes.objects[0]._id.toHexString());
+        assert.equal(findOneAndUpdateRes.objects[1].id,
+          findOneAndUpdateRes.objects[1]._id.toHexString());
+      });
+
+  });
 });
