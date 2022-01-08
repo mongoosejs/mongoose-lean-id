b22736ffd85c1be0cb76c2d5911bb2175871d185
Valeri Karpov <val@karpov.io>
2022-01-08 15:11:39 -0500

    feat: support Mongoose 6
    Fix #5

--- test/setup.test.js
+++ test/setup.test.js
@@ -3,7 +3,5 @@
 const mongoose = require('mongoose');
 
 before(function() {
-  mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-
-  mongoose.set('useFindAndModify', false);
+  mongoose.connect('mongodb://localhost:27017/test', {});
 });
